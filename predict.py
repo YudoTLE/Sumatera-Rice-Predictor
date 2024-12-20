f5397cab77dade681407ac009d40292c5f23c9ec
YudoTLE <>
2024-12-20 19:46:27 +0700

    initial commit

---
+++ predict.py
@@ -0,0 +1,24 @@
+import joblib, numpy as np
+from tensorflow.keras.models import load_model
+
+
+# Load the trained model
+model = load_model('src/model.keras')
+
+
+# Load the scaler
+scaler = joblib.load('src/scaler.pkl')
+
+
+# Prediction based on user input
+while True:
+    rainfall    = float(input('Rainfall (mm)   : '))
+    humidity    = float(input('Humidity (%)    : '))
+    temperature = float(input('Temperature (°C): '))
+
+    new_data = np.array([[rainfall, humidity, temperature]])
+
+    new_data_scaled = scaler.transform(new_data)
+    predicted_producion_per_harvest_area = model.predict(new_data_scaled)
+
+    print(f'Predicted production per harvest area (ton/ha):\n > { predicted_producion_per_harvest_area[0][0] }')
---
+++ src/train.py
@@ -0,0 +1,75 @@
+import os, pandas as pd, joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+
+# Set-up working directory
+WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+def resolve_path(filename):
+    return os.path.join(WORKING_DIRECTORY, filename)
+
+
+# Load dataset
+data = pd.read_csv(resolve_path('dataset.csv'))
+
+
+# Feature engineering
+data['Produksi_per_Luas_Panen'] = data['Produksi'] / data['Luas Panen']
+
+
+# Select the features and target variable
+X = data[['Curah hujan', 'Kelembapan', 'Suhu rata-rata']].values
+y = data['Produksi_per_Luas_Panen'].values
+
+
+# Normalize the features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+
+# Build the neural network model
+model = Sequential()
+
+
+# Neural Network Layers
+model.add(Dense(64, input_dim=3, activation='relu'))    # Input layer (3 features) + Hidden layer
+model.add(Dense(32, activation='relu'))                 # Hidden layer           
+model.add(Dense(1))                                     # Output layer (1 value: the prediction)
+
+
+# Compile the model
+model.compile(optimizer=Adam(), loss='mean_squared_error')
+
+
+# Train the model
+model.fit(X_train, y_train, epochs=128, batch_size=32, validation_split=0.2)
+
+
+# Evaluate the model
+predictions = model.predict(X_test)
+
+r2 = r2_score(y_test, predictions)
+mae = mean_absolute_error(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+
+with open(resolve_path('log.txt'), 'w') as f:
+    f.write(f'R Squared          : {r2}\n')
+    f.write(f'Mean Absolute Error: {mae}\n')
+    f.write(f'Mean Squared Error : {mse}\n')
+
+
+# Save the trained model
+model.save(resolve_path('model.keras'))
+
+
+# Save the scaler
+joblib.dump(scaler, resolve_path('scaler.pkl'))
